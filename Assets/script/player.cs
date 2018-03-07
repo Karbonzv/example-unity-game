3c9df97a2e3112e7e4cf511ba43e31f5cf89a8a5
Karbonzv <36472631+Karbonzv@users.noreply.github.com>
2018-03-07 10:50:16 +0000

    added functions

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class player : MonoBehaviour {
 
 	public string toPrintClass = "Hello world as class variable";
-
+	public float speed = 2;
 	// Use this for initialization
 	void Start () {
 		string toPrint = "Hello World as a variable!";
 		Debug.Log (toPrintClass);
+		string returned1 = TestFunction ("TestFunction message variable", 1);
+		string returned2 = TestFunction ("TestFunction message variable", 2);
+		Debug.Log ("returned1 = " + returned1);
+		Debug.Log ("returned2 = " + returned2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
+		float speed = 2;
+		ourRigidBody.velocity = Vector2.right*speed;
+	}
+
+	string TestFunction (string message, int count) {
+		Debug.Log (message + " " + count);
+
+		return "Returned string";
 	}
 }
