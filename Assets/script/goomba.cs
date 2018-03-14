bac8614a179566031c1dbacf14d089f3b86734db
Karbonzv <36472631+Karbonzv@users.noreply.github.com>
2018-03-14 10:51:57 +0000

    added damage dealing to goomba

---
+++ Assets/script/goomba.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goomba : MonoBehaviour {
+	
+	// public variables visible to editor
+	public float damage = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// called when trigger collides with another collider 
+	void OnTriggerStay2D(Collider2D other)
+	{
+		// attempting to get script from object collided with
+		player playerscript = other.GetComponent<player> ();
+
+		// if player script exits with object collded with
+		if (playerscript != null) {
+			// call damage function on player script and pas damage amount
+			playerscript.damage (damage);
+
+			Debug.Log ("Goomba dealt damage to player = " + damage);
+		} // end if (playerscript != null)
+	}// end ontrigger2D()
+}
--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -4,28 +4,55 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-	public string toPrintClass = "Hello world as class variable";
+//	public string toPrintClass = "Hello world as class variable";
 	public float speed = 2;
+	public float health = 100;
+	public float invulnerableDuration = 1;
+
+		private float invulnerableEndTime = 0;
+
 	// Use this for initialization
 	void Start () {
-		string toPrint = "Hello World as a variable!";
-		Debug.Log (toPrintClass);
-		string returned1 = TestFunction ("TestFunction message variable", 1);
-		string returned2 = TestFunction ("TestFunction message variable", 2);
-		Debug.Log ("returned1 = " + returned1);
-		Debug.Log ("returned2 = " + returned2);
+		//damage (5);
+
+
+		//string toPrint = "Hello World as a variable!";
+		//Debug.Log (toPrintClass);
+		//string returned1 = TestFunction ("TestFunction message variable", 1);
+		//string returned2 = TestFunction ("TestFunction message variable", 2);
+		//Debug.Log ("returned1 = " + returned1);
+		//Debug.Log ("returned2 = " + returned2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D> ();
 		float speed = 2;
-		ourRigidBody.velocity = Vector2.right*speed;
+		ourRigidBody.velocity = Vector2.right * speed;
 	}
 
-	string TestFunction (string message, int count) {
-		Debug.Log (message + " " + count);
+	//string TestFunction (string message, int count) {
+	//	Debug.Log (message + " " + count);
 
-		return "Returned string";
-	}
+		//return "Returned string";
+
+	public void damage (float damageToDeal)
+	{
+		if (Time.time >= invulnerableEndTime) {
+
+			// reducing health by damage passed in
+			health = health - damageToDeal;
+
+			//TODO: handle death
+
+			// set us as invulnerable for a set duration
+			invulnerableEndTime = Time.time + invulnerableDuration;
+
+			// log the result of function
+			Debug.Log ("Damage was dealt");
+			Debug.Log ("DamageToDo = " + damageToDeal);
+			Debug.Log ("health = " + health);
+		}
+	} // end damage()
+
 }
